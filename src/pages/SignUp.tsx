64b6b9a5d0a865e0c426ca6a08c56174b7c39043
Syed <72160793+ssa17@users.noreply.github.com>
2025-01-08 17:16:27 +0000

    fix usernames case-insensitive

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -26,8 +26,8 @@ export default function SignUp() {
     try {
       const { data, error } = await supabase
         .from('profiles')
-        .select('username'.toLowerCase())
-        .eq('username', username.toLowerCase)
+        .select('username')
+        .eq('username', username.toLowerCase())
         .maybeSingle();
 
       if (error) {
